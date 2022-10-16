e8aa854f7caf1c5962e1ef3e4960e9a98f1b3e12
HoroshoNaVolge <evgenij.kozlov@gmail.com>
2022-10-16 14:19:45 +0300

    Add InputLogic

--- EKozlov.FinalTestWork/Program.cs
+++ EKozlov.FinalTestWork/Program.cs
@@ -6,7 +6,20 @@ namespace EKozlov.FinalTestWork
     {
         static void Main(string[] args)
         {
-           
+            Random random = new Random();
+
+            int quantityOfElements = random.Next(3, 4);
+
+            string[] inputArray = new string[quantityOfElements];
+
+            Console.WriteLine($"Необходимо ввести {quantityOfElements} значений для формирования массива.");
+
+            for (int i = 0; i < quantityOfElements; i++)
+            {
+                Console.Write($"Введите {i + 1} значение: ");
+                inputArray[i] = Console.ReadLine();
+            }
+
         }
     }
 }
